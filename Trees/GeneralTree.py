ca257999a41bfa2070247cd6536d922f6d527277
Vinay Biradar <vinaybiradar1717@gmail.com>
2020-09-21 02:27:40 +0530

    general tree

---
+++ Trees/GeneralTree.py
@@ -0,0 +1,64 @@
+# |_ Music
+#   |_ Shaan
+#     |_ Bum Bum Bole
+#     |_ Chand Shifarish
+#   |_ Vijay
+#     |_ Hands Up
+#     |_ Gatiya Ilidu
+#     |_ Belageddu
+#   |_ Arijit
+#     |_ Tum Hi Ho
+#     |_ Janam Janam
+
+
+class Treenode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.parent = None
+
+    def AddChild(self, child):
+        child.parent = self
+        self.children.append(child)
+
+    def GetLevel(self):
+        level = 0
+        p = self.parent
+        while p:
+            level += 1
+            p = p.parent
+        return level
+
+    def PrintChildren(self):
+        spaces = " " * self.GetLevel()*2
+        prefix = spaces + "->"
+        print(f"{prefix} {self.data}")
+        if self.children:
+            for child in self.children:
+                child.PrintChildren()
+
+def BuildTree():
+    root = Treenode("Music")
+
+    Shaan = Treenode("Shaan")
+    Shaan.AddChild(Treenode("Bum Bum Bole"))
+    Shaan.AddChild(Treenode("Chand Shifarish"))
+
+    Vijay = Treenode("Vijay")
+    Vijay.AddChild(Treenode("Hands Up"))
+    Vijay.AddChild(Treenode("Gatiya Ilidu"))
+    Vijay.AddChild(Treenode("Belageddu"))
+
+    Arijit = Treenode("Arijit")
+    Arijit.AddChild(Treenode("Tum Hi Ho"))
+    Arijit.AddChild(Treenode("Janam Janam"))
+
+    root.AddChild(Shaan)
+    root.AddChild(Vijay)
+    root.AddChild(Arijit)
+
+    return root
+
+if __name__ == "__main__":
+    r = BuildTree()
+    r.PrintChildren()
